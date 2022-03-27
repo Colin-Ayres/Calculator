eb50edce1ce0d2b6fed90609d6afee8209b2b276
Colin Ayres <cayres7@gmail.com>
2022-03-27 19:51:48 -0400

    Added basic calculation functionality

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-topScreen = [];
-mainScreen = [];
+mainScreen = ["Awaiting input"];
+decimalCounter = 0;
+
 
 ///////////////////////////////////////////DOM Manipulation Begin///////////////////////////////////////////
 //Create container variables
@@ -10,6 +11,12 @@ const rowFour = document.querySelector(".rowFour");
 const rowFive = document.querySelector(".rowFive");
 const rowSix = document.querySelector(".rowSix")
 
+const mainScreenText = document.createElement("h1")
+mainScreenText.classList.add('mainScreenText')
+mainScreenText.innerHTML = mainScreen;
+rowOne.appendChild(mainScreenText);
+mainScreen = " ";
+
 //Create "mod" button
 const modButton = document.createElement("button");
 modButton.classList.add('modButton');
@@ -17,20 +24,23 @@ modButton.innerHTML = "%";
 modButton.type = "submit";
 modButton.name = "formBtn";
 modButton.onclick = function () {
-    //call mod operator
+    addOperator("%");
 }
 rowOne.appendChild(modButton);
 
-//Create "Clear everything (CE)" button
-const CEButton = document.createElement("button");
-CEButton.classList.add('CEButton');
-CEButton.innerHTML = "CE";
-CEButton.type = "submit";
-CEButton.name = "formBtn";
-CEButton.onclick = function () {
-    //Clear everything
+//Create "Divide by 0" button
+const dzButton = document.createElement("button");
+dzButton.classList.add('dzButton');
+dzButton.innerHTML = "Div by 0";
+dzButton.type = "submit";
+dzButton.name = "formBtn";
+dzButton.onclick = function () {
+    mainScreen = ["You can't divide by 0"];
+    mainScreenText.innerHTML = mainScreen;
+    resultHitCounter = 1;
+    mainScreen = " ";
 }
-rowOne.appendChild(CEButton);
+rowOne.appendChild(dzButton);
 
 //Create "Clear line (C)" button
 const clearButton = document.createElement("button");
@@ -39,7 +49,8 @@ clearButton.innerHTML = "C";
 clearButton.type = "submit";
 clearButton.name = "formBtn";
 clearButton.onclick = function () {
-    //Clear line
+    mainScreen = "Cleared, awaiting input";
+    mainScreenText.innerHTML = mainScreen; 
 }
 rowOne.appendChild(clearButton);
 
@@ -50,7 +61,8 @@ backspaceButton.innerHTML = "<--";
 backspaceButton.type = "submit";
 backspaceButton.name = "formBtn";
 backspaceButton.onclick = function () {
-    //add multiply to operator field
+    //mainScreen = mainScreen - mainScreen[mainScreen.length - 1];
+    //mainScreenText.innerHTML = mainScreen; 
 }
 rowOne.appendChild(backspaceButton);
 
@@ -61,7 +73,13 @@ inverseButton.innerHTML = "1/n";
 inverseButton.type = "submit";
 inverseButton.name = "formBtn";
 inverseButton.onclick = function () {
-    //call inverse operator
+    //execute operation immediately
+    if (noOperatorOnEnd() == true){
+        mainScreen = "inverse";
+        mainScreenText.innerHTML = mainScreen;
+        mainScreen = " ";
+        decimalCounter = 0;    
+    }
 }
 rowTwo.appendChild(inverseButton);
 
@@ -72,7 +90,16 @@ squaredButton.innerHTML = "n²";
 squaredButton.type = "submit";
 squaredButton.name = "formBtn";
 squaredButton.onclick = function () {
-    //Clear everything
+    //
+    //immediately square mainScreen and output result
+    //ResultHitcounter = 1;
+    if (noOperatorOnEnd() == true){
+        mainScreen = "squared";
+        mainScreenText.innerHTML = mainScreen;
+        mainScreen = " ";
+        decimalCounter = 0;    
+    }
+
 }
 rowTwo.appendChild(squaredButton);
 
@@ -83,18 +110,28 @@ sqrrootButton.innerHTML = "√";
 sqrrootButton.type = "submit";
 sqrrootButton.name = "formBtn";
 sqrrootButton.onclick = function () {
-    //Clear line
+    //if main screen only has numbers
+    //topScreen = sqrt(mainScreen)
+    //immediately square mainScreen and output result
+    //resulthitcounter = 1;
+    if (noOperatorOnEnd() == true){
+        mainScreen = "square root";
+        mainScreenText.innerHTML = mainScreen;
+        mainScreen = " ";
+        decimalCounter = 0;    
+    }
+
 }
 rowTwo.appendChild(sqrrootButton);
 
 //Create "/" button
 const divideButton = document.createElement("button");
 divideButton.classList.add('divideButton');
-divideButton.innerHTML = "/";
+divideButton.innerHTML = " / ";
 divideButton.type = "submit";
 divideButton.name = "formBtn";
 divideButton.onclick = function () {
-    //add multiply to operator field
+    addOperator("/");
 }
 rowTwo.appendChild(divideButton);
 
@@ -105,7 +142,8 @@ sevenButton.innerHTML = "7";
 sevenButton.type = "submit";
 sevenButton.name = "formBtn";
 sevenButton.onclick = function () {
-    //add 7 to number on display screen
+    addNumbertoScreen("7");
+
 }
 rowThree.appendChild(sevenButton);
 
@@ -116,7 +154,7 @@ eightButton.innerHTML = "8";
 eightButton.type = "submit";
 eightButton.name = "formBtn";
 eightButton.onclick = function () {
-    //add 2 to number on display screen
+    addNumbertoScreen("8");
 }
 rowThree.appendChild(eightButton);
 
@@ -127,18 +165,18 @@ nineButton.innerHTML = "9";
 nineButton.type = "submit";
 nineButton.name = "formBtn";
 nineButton.onclick = function () {
-    //add 9 to number on display screen
+    addNumbertoScreen("9");
 }
 rowThree.appendChild(nineButton);
 
 //Create "multiply" button
 const multiplyButton = document.createElement("button");
 multiplyButton.classList.add('multiplyButton');
-multiplyButton.innerHTML = "x";
+multiplyButton.innerHTML = " x ";
 multiplyButton.type = "submit";
 multiplyButton.name = "formBtn";
 multiplyButton.onclick = function () {
-    //add multiply to operator field
+    addOperator("*");
 }
 rowThree.appendChild(multiplyButton);
 
@@ -149,7 +187,7 @@ fourButton.innerHTML = "4";
 fourButton.type = "submit";
 fourButton.name = "formBtn";
 fourButton.onclick = function () {
-    //add 4 to number on display screen
+    addNumbertoScreen("4");
 }
 rowFour.appendChild(fourButton);
 
@@ -160,7 +198,7 @@ fiveButton.innerHTML = "5";
 fiveButton.type = "submit";
 fiveButton.name = "formBtn";
 fiveButton.onclick = function () {
-    //add 5 to number on display screen
+    addNumbertoScreen("5");
 }
 rowFour.appendChild(fiveButton);
 
@@ -171,18 +209,18 @@ sixButton.innerHTML = "6";
 sixButton.type = "submit";
 sixButton.name = "formBtn";
 sixButton.onclick = function () {
-    //add 6 to number on display screen
+    addNumbertoScreen("6");
 }
 rowFour.appendChild(sixButton);
 
 //Create "minus" button
 const minusButton = document.createElement("button");
 minusButton.classList.add('sixButton');
-minusButton.innerHTML = "-";
+minusButton.innerHTML = " - ";
 minusButton.type = "submit";
 minusButton.name = "formBtn";
 minusButton.onclick = function () {
-    //add minus to operator field
+    addOperator("-");
 }
 rowFour.appendChild(minusButton);
 
@@ -193,7 +231,7 @@ oneButton.innerHTML = "1";
 oneButton.type = "submit";
 oneButton.name = "formBtn";
 oneButton.onclick = function () {
-    //add 1 to number on display screen
+    addNumbertoScreen("1");
 }
 rowFive.appendChild(oneButton);
 
@@ -204,7 +242,7 @@ twoButton.innerHTML = "2";
 twoButton.type = "submit";
 twoButton.name = "formBtn";
 twoButton.onclick = function () {
-    //add 5 to number on display screen
+    addNumbertoScreen("2");
 }
 rowFive.appendChild(twoButton);
 
@@ -215,18 +253,18 @@ threeButton.innerHTML = "3";
 threeButton.type = "submit";
 threeButton.name = "formBtn";
 threeButton.onclick = function () {
-    //add 3 to number on display screen
+    addNumbertoScreen("3");
 }
 rowFive.appendChild(threeButton);
 
 //Create "plus" button
 const plusButton = document.createElement("button");
 plusButton.classList.add('plusButton');
-plusButton.innerHTML = "+";
+plusButton.innerHTML = " + ";
 plusButton.type = "submit";
 plusButton.name = "formBtn";
 plusButton.onclick = function () {
-    //add plus to operator field
+    addOperator("+");
 }
 rowFive.appendChild(plusButton);
 
@@ -237,7 +275,7 @@ oppositeButton.innerHTML = "+/-";
 oppositeButton.type = "submit";
 oppositeButton.name = "formBtn";
 oppositeButton.onclick = function () {
-    //make number opposite
+    addOperator("*(-1)");
 }
 rowSix.appendChild(oppositeButton);
 
@@ -248,7 +286,7 @@ zeroButton.innerHTML = "0";
 zeroButton.type = "submit";
 zeroButton.name = "formBtn";
 zeroButton.onclick = function () {
-    //add 0 to number on display screen
+    addNumbertoScreen("0");
 }
 rowSix.appendChild(zeroButton);
 
@@ -259,7 +297,17 @@ decimalButton.innerHTML = ".";
 decimalButton.type = "submit";
 decimalButton.name = "formBtn";
 decimalButton.onclick = function () {
-    //add decimal
+    if ((decimalCounter == 0) && (mainScreen[mainScreen.length-2] !== (")"))){
+        if (mainScreen[mainScreen.length - 1] == (" ")){
+            mainScreen = mainScreen + "0" + ".";
+            mainScreenText.innerHTML = mainScreen;
+            decimalCounter = 1;
+        } else if ((mainScreen !== (" ")) && ((mainScreen[mainScreen.length-2] !== ("%")) && (mainScreen[mainScreen.length-1] !== (".")) && (mainScreen[mainScreen.length-2] !== ("+")) && (mainScreen[mainScreen.length-2] !== ("-")) && (mainScreen[mainScreen.length-2] !== ("/")) && (mainScreen[mainScreen.length-2] !== ("*")))){
+            mainScreen = mainScreen + ".";
+            mainScreenText.innerHTML = mainScreen
+            decimalCounter = 1;
+        }
+    }
 }
 rowSix.appendChild(decimalButton);
 
@@ -270,11 +318,41 @@ equalButton.innerHTML = "=";
 equalButton.type = "submit";
 equalButton.name = "formBtn";
 equalButton.onclick = function () {
-    //add equal to operator field
+    if (noOperatorOnEnd() == true){
+        mainScreen = "equals";
+        mainScreenText.innerHTML = mainScreen;
+        mainScreen = " ";
+        decimalCounter = 0;    
+    }
 }
 rowSix.appendChild(equalButton);
 ///////////////////////////////////////////DOM Manipulation End///////////////////////////////////////////
 
+function noOperatorOnEnd(){
+    if ((mainScreen !== (" ")) && ((mainScreen[mainScreen.length-2] !== ("%")) && (mainScreen[mainScreen.length-1] !== (".")) && (mainScreen[mainScreen.length-2] !== ("+")) && (mainScreen[mainScreen.length-2] !== ("-")) && (mainScreen[mainScreen.length-2] !== ("/")) && (mainScreen[mainScreen.length-2] !== ("*")))){
+        console.log("true")
+        return true;
+        
+    } else {
+        console.log("false")
+        return false;
+    }
+}
+
+function addOperator(operatorInput){
+    if ((mainScreen !== (" ")) && ((mainScreen[mainScreen.length-2] !== ("%")) && (mainScreen[mainScreen.length-1] !== (".")) && (mainScreen[mainScreen.length-2] !== ("+")) && (mainScreen[mainScreen.length-2] !== ("-")) && (mainScreen[mainScreen.length-2] !== ("/")) && (mainScreen[mainScreen.length-2] !== ("*")))){
+        mainScreen = mainScreen + " " + operatorInput + " ";
+        mainScreenText.innerHTML = mainScreen;
+        decimalCounter = 0;
+    }
+}
+
+function addNumbertoScreen(numberInput){
+    if (mainScreen[mainScreen.length-2] !== (")")){
+    mainScreen = mainScreen + numberInput;
+    mainScreenText.innerHTML = mainScreen;
+    }
+}
 
 function add(numberOne, numberTwo){
     return numberOne + numberTwo;
@@ -291,12 +369,16 @@ function multiply(numberOne, numberTwo){
 function divide(numberOne, numberTwo){
     if (numberTwo == 0)
     {
-        return "ERROR DIV BY ZERO";
+        return "You can't divide by 0";
     }
     else 
     {
         return numberOne / numberTwo;
     }
+}
+
+function opposite(numberOne){
+    return numberOne * -1;
 }
 
 function mod(numberOne, numberTwo){
@@ -308,15 +390,23 @@ function operate(numberOne, numberTwo, inputOperator){
         add(numberOne, numberTwo);
     }
     else if (inputOperator == "-"){
-        subtract(numberOne,numberTwo)
+        subtract(numberOne,numberTwo);
     }
     else if (inputOperator == "*"){
-        multiply(numberOne,numberTwo)
+        multiply(numberOne,numberTwo);
     }
     else if (inputOperator == "/"){
-        divide(numberOne,numberTwo)
+        divide(numberOne,numberTwo);
     }
     else if (inputOperator == "%"){
-        mod(numberOne,numberTwo)
+        mod(numberOne,numberTwo);
     }
+    else if (inputOperator == "("){
+        opposite(numberOne);
+    }
+
 }
+
+//convert maintext to array, get rid of all spaces, collect number, operator, then number 2nd number
+//"("" ounts as operator then skip ahead 4 places for next operator) 
+//bubble search, execute to end of string
